ddaa169bc7f46b418f0a5aa96052a684c0102cb5
fractalbach <fractalbach@gmail.com>
2018-12-19 02:01:09 -0800

    added map edge collisions
    easiest type of collision: bounce the circles off the
    sides of the walls.  Currently, there are 100 random
    circles generated and bouncing all over the place.
    
    Later, there will be collisions between the
    circles themselves.

--- main.js
+++ main.js
@@ -7,6 +7,22 @@ Math['randint'] = (min, max)=>{
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+Math['randintNorm'] = (min, max)=> {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let r = (Math.random() + Math.random()) / 2;
+    return Math.floor(r * (max - min + 1)) + min;
+};
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 // global object for debugging convenience.
 let game = {};
 
@@ -18,16 +34,21 @@ class Circle {
         this.x = x;
         this.y = y;
         this.r = r;
+        this.vx = 0;
+        this.vy = 0;
         this.strokeStyle = 'black';
         this.fillStyle = 'white';
-        this.path = new Path2D();
-        this.path.arc(x, y, r, 0, 2*Math.PI);
+        // this.path = new Path2D();
+        // this.path.arc(x, y, r, 0, 2*Math.PI);
     }
     draw(ctx) {
         ctx.strokeStyle = this.strokeStyle;
         ctx.fillStyle = this.fillStyle;
-        ctx.fill(this.path);
-        ctx.stroke(this.path);
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
     }
 }
 
@@ -40,9 +61,38 @@ class Scene {
     add(key, item) {
         this.objects.set(key, item);
     }
+    clear() {
+        let w = this.canvas.width;
+        let h = this.canvas.height;
+        this.ctx.clearRect(0, 0, w, h);
+    }
     display() {
-        for (let [k,v] of this.objects.entries()) {
+        for (let v of this.objects.values()) {
             v.draw(this.ctx);
+        }
+    }
+    update() {
+        let w = this.canvas.width;
+        let h = this.canvas.height;
+        let toohigh = false;
+        let toolow = false;
+        for (let o of this.objects.values()) {
+            o.x = o.x + o.vx;
+            o.y = o.y + o.vy;
+            toohigh = (o.x > w);
+            toolow = (o.x < 0);
+            if (toohigh===true || toolow===true) {
+                if (toohigh) {o.x = w - 2*(o.x - w);}
+                if (toolow)  {o.x = -o.x;}
+                o.vx = (-o.vx)
+            }
+            toohigh = (o.y > h);
+            toolow = (o.y < 0);
+            if (toohigh===true || toolow===true) {
+                if (toohigh) {o.y = h - 2*(o.y - h);}
+                if (toolow)  {o.y = -o.y;}
+                o.vy = (-o.vy)
+            }
         }
     }
 }
@@ -52,13 +102,13 @@ function addRandomCircles(scene) {
     let h = scene.canvas.height;
     let n = 100;
     for (let i=0; i<n; i++) {
-        let r = Math.randint(10, 20);
+        let r = Math.randintNorm(w/100, w/25);
         let x = Math.randint(0+r, w-r);
         let y = Math.randint(0+r, h-r);
         let o = new Circle(x, y, r);
-        if (i%5 == 0) {
-            o.fillStyle = 'rgba(0, 0, 50, 50)';
-        }
+        o.fillStyle = getRandomColor();
+        o.vx = (2*Math.random()-0.5) * 3;
+        o.vy = (2*Math.random()-0.5) * 3;
         let key = i;
         scene.add(key, o);
     }
@@ -69,8 +119,15 @@ function main() {
     addRandomCircles(scene);
     scene.display();
     game = scene;
-}
 
+    function loop() {
+        scene.update();
+        scene.clear();
+        scene.display();
+        window.requestAnimationFrame(loop);
+    }
+    loop();
+}
 
 
 window.addEventListener('load', main, false);
